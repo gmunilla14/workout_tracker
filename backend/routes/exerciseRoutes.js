aeda571c48b0d2c981dc1655abecd2e20aa91515
Greg Munilla <gregmunilla@gmail.com>
2021-12-17 21:54:33 -0500

    Create exercise routes

---
+++ backend/routes/exerciseRoutes.js
@@ -0,0 +1,40 @@
+const { Exercise } = require("../models/exercise");
+const express = require("express");
+const Joi = require("joi");
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  try {
+    const exercises = await Exercise.find();
+
+    res.send(exercises);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+router.post("/", async (req, res) => {
+  const schema = Joi.object({
+    name: Joi.string().required(),
+    muscleIDs: Joi.array().items(Joi.number()).required(),
+  }).options({ abortEarly: false });
+
+  const { error } = schema.validate(req.body);
+  if (error) return res.status(500).send(error.details[0].message);
+
+  const { name, muscleIDs } = req.body;
+
+  let exercise = new Exercise({
+    name,
+    muscleIDs,
+  });
+
+  try {
+    exercise = await exercise.save();
+    res.send(exercise);
+  } catch (error) {
+    res.status(500).send(error.message);
+    console.log(error.message);
+  }
+});
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const planRoutes = require("./routes/planRoutes");
+const exerciseRoutes = require('./routes/exerciseRoutes')
 
 dotenv.config();
 
@@ -12,6 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/plans", planRoutes);
+app.use('/api/exercises', exerciseRoutes)
 
 const connection_string = process.env.CONNECT_STRING;
 const port = process.env.PORT || 5000;
