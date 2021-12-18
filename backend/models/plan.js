fbe7007156ea5b4a793911c0f1928e3e49c983b7
Greg Munilla <gregmunilla@gmail.com>
2021-12-17 20:41:06 -0500

    Create workout plan schema

---
+++ backend/models/plan.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const planSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 100,
+  },
+  creatorID: {
+    type: String,
+    required: true
+  },
+  sets: [
+    {
+      number: { type: Number, required: true },
+      type: { type: String, required: true },
+      plannedWeight: Number,
+      plannedReps: Number,
+      exerciseID: String,
+      plannedDuration: Number,
+      exerciseID: String
+    },
+  ],
+});
+
+const Plan = mongoose.model("Plan", planSchema);
+exports.Plan = Plan;
