ac9d6042e51ec1f503061afe9f1941646abf7db8
Greg Munilla <gregmunilla@gmail.com>
2021-12-17 20:41:19 -0500

    Create basic server

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const planRoutes = require("./routes/planRoutes");
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/plans", planRoutes);
+
+const connection_string = process.env.CONNECT_STRING;
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}...`);
+});
+
+mongoose
+  .connect(connection_string, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB connection established");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
